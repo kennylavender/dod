0a48be63bab808aba22b3b6edbd8ef098675740a
Kenny Lavender <leapingfish@gmail.com>
2016-10-28 18:41:45 -0700

    modify the notice and make the javascript more reliable

--- src/index.js
+++ src/index.js
@@ -4,4 +4,76 @@ import './index.css';
 import './bower_components/font-awesome/css/font-awesome.css'
 import './Intro.css'
 import './Intro.js'
-import './notice.js'
+import Notice from './notice.js'
+
+var DEBUG = false;
+
+var testDate = {};
+testDate.start = new Date();
+testDate.start = testDate.start.setSeconds(testDate.start.getSeconds() - 1)
+testDate.end = new Date();
+testDate.end = testDate.end.setSeconds(testDate.end.getSeconds() + 30);
+
+var x = new Date();
+var offset = x.getTimezoneOffset() / 60;
+
+var dates = [
+  {'start': Date.UTC(2016, 9, 22, 17 + offset, 0), end: Date.UTC(2016, 9, 22, 22 + offset, 30)},
+  {'start': Date.UTC(2016, 9, 28, 17 + offset, 0), end: Date.UTC(2016, 9, 28, 22 + offset, 30)},
+  {'start': Date.UTC(2016, 9, 29, 17 + offset, 0), end: Date.UTC(2016, 9, 29, 22 + offset, 30)},
+];
+
+if ( DEBUG ) dates.push(testDate)
+
+var notice = Notice({dates: dates})
+var noticeEl = document.getElementById('notice');
+var noticeDetails = document.getElementById('notice-details')
+
+var noticeDetailsClose = document.getElementById('notice-details__close');
+var noticeDetailsToggle = document.getElementById('notice-details__toggle');
+
+var hideNoticeDetails = function () {
+    noticeDetails.classList.remove('is-open');
+}
+var showNoticeDetails = function () {
+    noticeDetails.classList.add('is-open');
+}
+var toggleNoticeDetails = function (e) {
+    e.preventDefault();
+    if (noticeDetails.classList.contains('is-open')) {
+        hideNoticeDetails();
+    } else {
+        showNoticeDetails();
+    }
+}
+
+noticeDetailsClose.addEventListener('click', function(e) { toggleNoticeDetails(e) }, false);
+noticeDetailsToggle.addEventListener('click', function(e) { toggleNoticeDetails(e) }, false);
+
+
+var showNotice = function() {
+    if ( DEBUG ) console.log('Show Notice')
+    if (noticeEl) {
+        noticeEl.classList.remove('hide');
+    }
+}
+var hideNotice = function () {
+    if ( DEBUG ) console.log('Hide Notice')
+    if(noticeEl) {
+        noticeEl.classList.add('hide');
+    }
+}
+
+var check = function() {
+    if (notice.run(Date.now())) {
+        showNotice()
+    } else {
+        hideNotice()
+    }
+}
+var timer = function() {
+    setTimeout( function(){ check(); timer(); }, 6000)
+}
+
+check();
+timer();
