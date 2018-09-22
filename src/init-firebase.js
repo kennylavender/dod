a8f7f66143dd2651987ca0c355ca2ed00088ef16
Kenny Lavender <5175120+kennylavender@users.noreply.github.com>
2018-09-22 14:05:07 -0700

    prettier

--- src/init-firebase.js
+++ src/init-firebase.js
@@ -1,14 +1,14 @@
-const firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/database");
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
 
 var config = {
-  apiKey: "AIzaSyDf9o8qNtsdHNZnMIJJ1gULesjuru2ysFY",
-  authDomain: "dreams-of-darkness.firebaseapp.com",
-  databaseURL: "https://dreams-of-darkness.firebaseio.com",
-  projectId: "dreams-of-darkness",
-  storageBucket: "dreams-of-darkness.appspot.com",
-  messagingSenderId: "408375047456"
+  apiKey: 'AIzaSyDf9o8qNtsdHNZnMIJJ1gULesjuru2ysFY',
+  authDomain: 'dreams-of-darkness.firebaseapp.com',
+  databaseURL: 'https://dreams-of-darkness.firebaseio.com',
+  projectId: 'dreams-of-darkness',
+  storageBucket: 'dreams-of-darkness.appspot.com',
+  messagingSenderId: '408375047456',
 };
 
 firebase.initializeApp(config);
--- src/sold-out-flag.js
+++ src/sold-out-flag.js
@@ -1,18 +1,21 @@
-const firebase = require("firebase/app");
-require("firebase/database");
+const firebase = require('firebase/app');
+require('firebase/database');
 
 const soldOutEl = document.getElementById('sold-out-flag');
-const isVisibleClass = 'is-visible'
+const isVisibleClass = 'is-visible';
 
 const showSoldOut = () => soldOutEl.classList.add(isVisibleClass);
 const hideSoldOut = () => soldOutEl.classList.remove(isVisibleClass);
 
-const toggleSoldOut = (isSoldOut) => isSoldOut ? showSoldOut() : hideSoldOut()
+const toggleSoldOut = isSoldOut => (isSoldOut ? showSoldOut() : hideSoldOut());
 
 const getIsSoldOut = x => x.isSoldOut;
 
-const getSnapshotValue = x => x.val()
+const getSnapshotValue = x => x.val();
 
-const onSoldOutChange = x => toggleSoldOut(getIsSoldOut(getSnapshotValue(x)))
+const onSoldOutChange = x => toggleSoldOut(getIsSoldOut(getSnapshotValue(x)));
 
-firebase.database().ref('/soldOut').on('value', onSoldOutChange)
+firebase
+  .database()
+  .ref('/soldOut')
+  .on('value', onSoldOutChange);
